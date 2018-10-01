414846cd508e433c576b3bd5c6f97c0fd7889b9a
kimdognmin1 <43715435+kimdognmin1@users.noreply.github.com>
2018-10-01 21:02:23 +0900

    Add files via upload

---
+++ led.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(20, GPIO.OUT)
+GPIO.output(20, False)
+GPIO.setup(21, GPIO.OUT)
+GPIO.output(21, False)
+
+GPIO.output(20, True)
+GPIO.output(21, True)
+
+time.sleep(1)
+
+GPIO.output(20, False)
+GPIO.output(21, False)
+
+
+
